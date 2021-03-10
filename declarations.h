81a6f7af28c4276d41ee3c8cdef5f83b263fd4f4
int-GetName <79903723+int-GetName@users.noreply.github.com>
2021-03-10 19:49:05 +0800

    Create declarations.h
    Add the file that contains the declarations of the routines needed and some constants and the includes of other needed headers.

---
+++ declarations.h
@@ -0,0 +1,35 @@
+#ifndef __GREEDY_H__
+#define __GREEDY_H__
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<unistd.h>
+#include<utility>
+#include<deque>
+#include<stdio.h>
+#include<ncurses.h>
+#include<cstdbool>
+using namespace std;
+//the width should be double of the length
+#define WIDTH 40
+#define LENGTH (WIDTH/2)
+#define UPPERBOUND (string(1,'+')+string(WIDTH-2,'-')+string("+\n"))
+#define INIT_LENGTH 3
+
+struct snake{
+    snake();
+    //Data members
+    deque<pair<int,int>> body;
+    char direc;
+};
+
+void init_snake(const snake&,vector<string>&);
+void init_pixels(vector<string>&);
+
+bool move_snake(snake&,vector<string>&);
+void refresh(const vector<string>&);
+void take_control(void);
+char timed_getch(void);
+
+#endif //!__GREEDY_H__
